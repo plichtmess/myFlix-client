d8b3d83a687ad53e8b104d4e1e132fa520c44527
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-03-22 15:35:19 +0100

    deleted propTypes for genre and director

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,8 +15,6 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    genre: PropTypes.string,
-    director: PropTypes.string,
     description: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
