fdd84aa49a880f30f855fe41f16071aeb1f2f687
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-03-30 15:14:48 +0200

    gave classes to the title and text on movie cards and applied css styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,29 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+import "./movie-card.scss";
+
+// export const MovieCard = ({ movie, onMovieClick }) => {
+//   return (
+//     <div
+//       onClick={() => {
+//         onMovieClick(movie)
+//       }}
+//     >
+//       {movie.title}
+//     </div>
+//   );
+// };
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie)
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card className="h-100" onClick={() => onMovieClick(movie)} variant="link">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title className="card-title">{movie.title}</Card.Title>
+        <Card.Text className="card-text">{movie.director}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
