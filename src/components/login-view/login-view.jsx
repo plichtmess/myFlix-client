d748b9a4917bd2f37096525e697cb89ccd1d0be0
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-03-30 15:15:04 +0200

    imported Button and Form from react bootstrap and rearranged my code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
@@ -38,28 +40,53 @@ export const LoginView = ({ onLoggedIn }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
+    // <form onSubmit={handleSubmit}>
+    //   <label>
+    //     Username:
+    //     <input
+    //       type="text"
+    //       value={username}
+    //       onChange={(e) => setUsername(e.target.value)}
+    //       required
+    //       minLength="5"
+    //     />
+    //   </label>
+    //   <label>
+    //     Password:
+    //     <input
+    //       type="password"
+    //       value={password}
+    //       onChange={(e) => setPassword(e.target.value)}
+    //       required
+    //       minLength="5"
+    //     />
+    //   </label>
+    //   <button type="submit">Submit</button>
+    // </form>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
-          minLength="5"
+          minLength="3"
         />
-      </label>
-      <label>
-        Password:
-        <input
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          minLength="5"
         />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">Submit</Button>
+
+    </Form>
   );
 };
