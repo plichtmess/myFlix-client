ad1f095f1f8918dbd8727c5618cca9f30a38da04
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-12 19:07:52 +0200

    added favorite movies to profile page

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Row, Col, Container, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = (user, token) => {
+export const ProfileView = ({ movies }) => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
 
-
-
+  let favoriteMovies = () => {
+    if (movies) {
+      movies.filter(m => user.FavoriteMovies.includes(m._id));
+    }
+  };
 
   return (
     <Container>
@@ -14,12 +22,15 @@ export const ProfileView = (user, token) => {
           <Card.Body>
             <Card.Title className="card-title">{user.Username}</Card.Title>
             <Card.Text>Email: {user.Email}</Card.Text>
-            <Card.Text>Birthday: {user.Birthday}</Card.Text>
+            <Card.Text type="date">Birthday: {user.Birthday}</Card.Text>
             <Link to="/users/update">
               <Button variant="link">Edit User Profile</Button>
             </Link>
           </Card.Body>
         </Card>
+      </Row>
+      <Row>
+        <h1>Favorite Movies</h1>
       </Row>
     </Container>
   )
