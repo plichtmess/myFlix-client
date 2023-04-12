cb037220ca4f6f5ef6425e5b87555fcf1dc9c493
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-12 13:42:36 +0200

    imported the profile view and user update component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,8 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { ProfileView } from "../profile-view/profile-view";
+import { UserUpdate } from "../profile-view/user-update";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
