2c40920852e12198ef6ba25e83d22f16e61d22db
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-12 19:07:29 +0200

    added methods to add and delete movies from favorites

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,48 @@ import { Link } from "react-router-dom";
 
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, token }) => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+
+  const addFav = () => {
+    fetch(`https://myflixphilipp.herokuapp.com/users/${storedUser.username}/movies/${movie.id}`, {
+      method: "PUT",
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Failed adding movie to Favorites");
+        }
+      })
+      .then(user => {
+        if (user) {
+          alert("Movie was added to favorites");
+        }
+      })
+  };
+
+  const deleteFav = () => {
+    fetch(`https://myflixphilipp.herokuapp.com/users/${storedUser.Username}/movies/${movie.id}`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Failed deleting favorite movie");
+        }
+      })
+      .then(user => {
+        if (user) {
+          alert("Movie was deleted from favorites");
+        }
+      });
+  };
+
   return (
     <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
       <Card className="h-100" variant="link">
@@ -13,6 +54,8 @@ export const MovieCard = ({ movie }) => {
         <Card.Body>
           <Card.Title className="card-title">{movie.title}</Card.Title>
           <Card.Text className="card-text">{movie.director}</Card.Text>
+          <Button onClick={addFav}>Add to Favorites</Button>
+          <Button onClick={deleteFav}>Delete from Favorites</Button>
         </Card.Body>
       </Card>
     </Link>
