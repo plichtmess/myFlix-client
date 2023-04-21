d76f8b86acf46fc39c7c5b3af239d144f0d7a846
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-04-21 16:01:05 +0200

    added some styling

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,12 +4,7 @@ import { Row, Col, Container, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { MovieCard } from "../movie-card/movie-card";
 
-export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
+export const ProfileView = ({ user, movies }) => {
 
   console.log(movies);
   console.log(user);
@@ -22,6 +17,9 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
   return (
     <Container>
       <Row>
+        <Col>
+          <h1>Account Info</h1>
+        </Col>
         <Card className="h-100">
           <Card.Body>
             <Card.Title className="card-title">{user.Username}</Card.Title>
@@ -35,8 +33,10 @@ export const ProfileView = ({ user, token, movies, onLoggedOut }) => {
       </Row>
       <Row>
         <Col>
-          <h1>Favorite Movies</h1>
+          <h3>Favorite Movies</h3>
         </Col>
+      </Row>
+      <Row>
         {favoriteMovies.map((movie) => (
           <Col className="mb-4" key={movie.id} md={3}>
             <MovieCard movie={movie} />
